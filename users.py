257ec8ea69edb10032d45e3d17822294d6b7bd3d
pae <pablo.echegaray97@gmail.com>
2023-04-05 21:43:18 -0300

    post method

--- users.py
+++ users.py
@@ -28,9 +28,22 @@ async def user(id: int):
 async def user(id: int):
     return search_user(id)
 
+#POST
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"Error:" "El usuario ya existe"}
+    else:
+        users_list.append(user)
+
+
+
+
 def search_user(id: int):
     users = filter(lambda user: user.id == id, users_list)
     try:
         return list(users)[0]
     except:
         return {"Error": "No se ha encontrado el usuario"}
+
+
