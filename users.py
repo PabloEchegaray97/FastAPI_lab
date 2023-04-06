07dc28b1d57595f43ebb11a7bdd00e436603619b
pae <pablo.echegaray97@gmail.com>
2023-04-06 07:38:46 -0300

    put method

--- users.py
+++ users.py
@@ -36,7 +36,18 @@ async def user(user: User):
     else:
         users_list.append(user)
 
+#PUT
+@app.put("/user/")
+async def user(user: User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        return {"Error": "No se ha encontrado el usuario"}
 
+#DELETE
 
 
 def search_user(id: int):
